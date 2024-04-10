013e0bd649c199c0224d2ac71a44764ef64cc57c
longphat12 <longphat12022903@gmail.com>
2024-04-10 09:30:09 +0700

    mester

--- resources/views/layouts/client/master.blade.php
+++ resources/views/layouts/client/master.blade.php
@@ -7,20 +7,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Client</title>
     @stack('css')
+    @vite('resources/css/app.css')
     <link rel="stylesheet" href="{{ asset('css/client/product-list.css') }}">
     <link rel="stylesheet" href="{{ asset('css/client/header.css') }}">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
         integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
-    <div class="wrapper" style="background-color: #f4f6f8;">
+    <div class="wrapper" >
         @include('layouts.client.header')
-        <div class="content container" style="padding-top: 120px;">
+        <div class="content container pt-20">
             <div class="row">
                 <div class="col-12">
                     @yield('content')
@@ -31,7 +33,9 @@
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     @stack('js')
+
 </body>
 
 </html>
